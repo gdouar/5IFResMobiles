433932998764949e016c5db90541596e27a5f122
L P <poisse.lucas@gmail.com>
2018-10-12 14:54:49 +0200

    AJOUT script création bdd

---
+++ Server-Backend/dbcreation.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Reseaux(
+    IdReseau INTEGER PRIMARY KEY, 
+    SSID VARCHAR(32),
+    Type VARCHAR(50)
+ );
+ 
+ CREATE TABLE Mesures(
+   IdMesure INTEGER PRIMARY KEY,
+   Latitude FLOAT,
+   Longitude FLOAT,
+   DateMesure DATE,
+   BandePasssante FLOAT,
+   ForceSignal INTEGER,
+   IdReseau INTEGER
+  )
+
+ALTER TABLE Mesures ADD CONSTRAINT fk_MesureReseaux FOREIGN KEY (idReseau) REFERENCES reseaux(idReseau)
+    
