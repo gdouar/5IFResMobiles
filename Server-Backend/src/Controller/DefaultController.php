9962baa84c33844f567deb3437848e9926fb8955
L P <poisse.lucas@gmail.com>
2018-10-15 22:33:09 +0200

    MAJ basic nw selection service

--- Server-Backend/src/Controller/DefaultController.php
+++ Server-Backend/src/Controller/DefaultController.php
@@ -4,38 +4,54 @@ namespace App\Controller;
 
 use App\Entity\Mesures;
 use App\Entity\Reseaux;
+use Doctrine\ORM\Query\ResultSetMapping;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 /**
  * Class DefaultController
  *
- * Classe de test par défaut
+ * Contrôleur central
  * @package App\Controller
  */
 class DefaultController extends Controller
 {
+    //TODO refactor 
+    private $serializer;
+
     function __construct()
     {
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $this->serializer = new Serializer($normalizers, $encoders);
     }
 
     /**
-     * @return Response
-     * @throws \Exception
+     * Retourne les différents réseaux et mesures correspondantes au format JSON.
+     * TODO ajouter les filtres utilisateur
+     * @return Response JSON la réponse backend
+     * @throws \Exception cas d'erreurs variables
      */
-    public function index()
+    public function getAllNetworksAndMeasures()
     {
-        $id = 1;
-        //$number = random_int(0, 10);
-        $reseau = $this->getDoctrine()
-        ->getRepository(Reseaux::class)
-        ->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $rsm = new ResultSetMappingBuilder($em);  
+        $rsm->addRootEntityFromClassMetadata(Reseaux::class, 'r');
+        //$rsm->addJoinedEntityFromClassMetadata(Mesures::class, 'm', 'r', 'mesures');
+        //TODO erreur ORM lorsqu'une jointure est faite => vérifier le mapping ? 
 
-        if (!$reseau) {
-            throw 'No reseau found for id '.$id;
-        }
-        return new Response(
-            '<html><body>test: ' . $id . '</body></html>'
-        );
+        $selectClause = $rsm ->generateSelectClause();
+        $queryString = 'SELECT '.$selectClause.' from reseaux r '; //.'INNER JOIN mesures m on r.idreseau = m.idreseau';
+        $query = $em->createNativeQuery($queryString, $rsm);
+        $networks = $query->getResult();s
+        $jsonNetworks = ($this->serializer)->serialize($networks, 'json');
+        $response = new Response($jsonNetworks);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 }
