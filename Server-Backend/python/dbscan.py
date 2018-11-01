cc05f92a5ba07f9a7fb9f949ae666f3a6b2518cd
L P <poisse.lucas@gmail.com>
2018-11-01 18:23:58 +0100

    FIX stupid shell character limits // TODO fix race condition in DBScanClustering

--- Server-Backend/python/dbscan.py
+++ Server-Backend/python/dbscan.py
@@ -1,46 +1,51 @@
 # script de clustering des mesures d'un réseau donné.
+from __future__ import with_statement
 import json
 import sys
 import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sklearn.preprocessing import MinMaxScaler
+import os
+
 DBSCAN_EPS = 0.4
 DBSCAN_MINPOINTS = 2
-
-if len(sys.argv) >= 2:
-    strData=""
-    i=1
-    while i < len(sys.argv):        # reconstruction des arguments
-        strData += " " + str(sys.argv[i])
-        i = i+1
-    data=json.loads(strData)
-    #data=testData
-    if len(data) > 0:
-        X = np.empty(shape=((len(data)), 3))
-        for idx, mesure in enumerate(data):
-            X[idx][0] = mesure["latitude"]
-            X[idx][1] = mesure["longitude"]
-            X[idx][2] = mesure["bandepassante"]
-        scaler = MinMaxScaler()
-        scaler.fit(X)
-        #print(X)
-        #print(scaler.transform(X))
-        X = scaler.transform(X)
-        clustering = DBSCAN(eps=DBSCAN_EPS, min_samples=DBSCAN_MINPOINTS).fit(X)
-        result = clustering.labels_
-        clusters2mesures = {}
-        for idx, cluster in enumerate(result):
-            strCluster = "zone" + str(cluster) 
-            if not(strCluster in clusters2mesures):
-                clusters2mesures[strCluster] = []
-            clusters2mesures[strCluster].append(data[idx])
-        #print(X)
-        print(json.dumps(clusters2mesures))
-        #print(len(X))
-        #print(result)
-    else:   
-        print(json.dumps({}))
-else:
-    raise Exception('Aucun input JSON détecté')
-
+try:
+    if len(sys.argv) >= 2:
+        with open(sys.argv[1], "r") as data:
+            data = data.read()
+            if data != "":
+                data = json.loads(data)
+                #data=testData
+                if len(data) > 0:
+                    X = np.empty(shape=((len(data)), 3))
+                    for idx, mesure in enumerate(data):
+                        X[idx][0] = mesure["latitude"]
+                        X[idx][1] = mesure["longitude"]
+                        X[idx][2] = mesure["bandepassante"]
+                    scaler = MinMaxScaler()
+                    scaler.fit(X)
+                    #print(X)
+                    #print(scaler.transform(X))
+                    X = scaler.transform(X)
+                    clustering = DBSCAN(eps=DBSCAN_EPS, min_samples=DBSCAN_MINPOINTS).fit(X)
+                    result = clustering.labels_
+                    clusters2mesures = {}
+                    for idx, cluster in enumerate(result):
+                        strCluster = "zone" + str(cluster) 
+                        if not(strCluster in clusters2mesures):
+                            clusters2mesures[strCluster] = []
+                        clusters2mesures[strCluster].append(data[idx])
+                    #print(X)
+                    print(json.dumps(clusters2mesures))
+                    #print(len(X))
+                    #print(result)
+                else:   
+                    print(json.dumps({}))
+            else: 
+                print("ERREUR: fichier vide")
+    else:
+        print("ERREUR : chemin du fichier non fourni")
+except EnvironmentError as e:  # parent of IOError, OSError *and* WindowsError where available
+    print('ERREUR : fichier de donnees data.json inacessible')
+    print(e)
--- Server-Backend/src/Service/DBScanClustering.php
+++ Server-Backend/src/Service/DBScanClustering.php
@@ -5,6 +5,8 @@ use App\Util\CommandsUtil;
 
 class DBScanClustering extends BaseClustering
 {
+    private static $factory;
+
     function __construct($matrix)
     {
         parent::__construct($matrix);
@@ -16,11 +18,29 @@ class DBScanClustering extends BaseClustering
     public function cluster()
     {
         $encodedMatrix = json_encode($this->measuresMatrix);
-        $encodedMatrix = str_replace("\"", "\\\"", $encodedMatrix);
-        // pirouette pour faire tout passer en ligne de commande
-
-        $command = CommandsUtil::getPythonShellCommand() . " " . __DIR__ . "/../../python/dbscan.py " . $encodedMatrix;
-        $output  = shell_exec($command);
+        $pythonDir = __DIR__ . "/../../python/";
+        $dataFile = "data.json";
+        $fullPathDataFile = $pythonDir . $dataFile;
+        if(!file_exists($fullPathDataFile)){
+            $fp = fopen($fullPathDataFile , "w");
+        }
+        else {
+            $fp = fopen($fullPathDataFile, "r+");
+        }
+        if (flock($fp, LOCK_EX )) { // acquière un verrou exclusif
+            ftruncate($fp, 0);     // effacement du contenu
+            fwrite($fp, $encodedMatrix);
+            fflush($fp);            // libère le contenu avant d'enlever le verrou
+            flock($fp, LOCK_UN);    // Enlève le verrou
+            
+            //TODO corriger bug de synchro ici, éventuellement utiliser des mutex ? => difficile car pas supportées sous Windows >:(
+            $command = CommandsUtil::getPythonShellCommand() . " " . $pythonDir . "dbscan.py " . $fullPathDataFile;
+            $output  = shell_exec($command); 
+            
+            fclose($fp);
+        } else {
+            echo "Impossible de verrouiller le fichier dump !";
+        }
         $result  = json_decode($output);
         return $result;
     }
