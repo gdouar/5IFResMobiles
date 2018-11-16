638ff9f4d80324b28a1bf156ada4dcd8ede70eaf
L P <poisse.lucas@gmail.com>
2018-11-16 22:34:47 +0100

    MAJ moyenne par jour dans les graphiques détails

--- FrontEnd/src/pages/details/details.ts
+++ FrontEnd/src/pages/details/details.ts
@@ -3,6 +3,7 @@ import {Mesure} from "../../model/Mesure.model";
 import { ColorsUtil } from '../../util/ColorsUtil';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Chart} from 'chart.js';
+import { dateValueRange } from 'ionic-angular/umd/util/datetime-util';
 @Component({
   selector: 'page-details',
   templateUrl: 'details.html',
@@ -23,20 +24,36 @@ export class DetailPage {
     console.log(this.mesure)
     this.reseau=this.mesure.reseau.ssid;
   }
+  averageDayResults(mesureProperty){
+    var measureMap = new Map();
+    var that = this;
+    that.mesure.mesuresPassees.forEach(value => {
+        var date = new Date(value.datemesure.date).toString()
+        if(measureMap.has(date)){
+            measureMap.set(date, measureMap.get(date) + value[mesureProperty]);
+        }
+        else measureMap.set(date, value.bandepassante);
+    })
+    console.log(measureMap);
+    var dataArray = new Array();
+    measureMap.forEach((value, key) => {     
+        var dateArr = new Date(key);
+        var findArr = that.mesure.mesuresPassees.filter(
+            mes => new Date(mes.datemesure.date).toString() == key);
+        dataArray.push(<any>{x: dateArr, y:value / (findArr.length)});
+    });
+    console.log(dataArray); 
+    return dataArray;
+  }
   ionViewDidLoad(){
     console.log(this.lineCanvas)
     var that = this;
-    var dataBandwidth = that.mesure.mesuresPassees.map(mes => {
-        return {
-            x: new Date(mes.datemesure.date),
-            y: mes.bandepassante
-        };
-    })
+    console.log(that.mesure.mesuresPassees);
     this.lineChart = new Chart(this.lineCanvas.nativeElement, {
         type: 'line',
         data: {
             datasets: [{
-                data: dataBandwidth,
+                data:  that.averageDayResults("bandepassante"),
                 fill: false,
                 backgroundColor: ["#000000"],
                 borderColor: "#000000"
@@ -66,17 +83,11 @@ export class DetailPage {
         }
 
     });
-    var signalBandwidth = that.mesure.mesuresPassees.map(mes => {
-        return {
-            x: new Date(mes.datemesure.date),
-            y: mes.forcesignal
-        };
-    })
     this.lineChartSignal = new Chart(this.lineCanvasSignal.nativeElement, {
         type: 'line',
         data: {
             datasets: [{
-                data: signalBandwidth,
+                data:  that.averageDayResults("forcesignal"),
                 fill: false,
                 backgroundColor: ["#000000"],
                 borderColor: "#000000"
@@ -87,11 +98,6 @@ export class DetailPage {
                 display:false
             },
             scales: {
-            /*    yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }],  */
                 xAxes: [{
                     type: 'time',
                     time: {
